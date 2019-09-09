9645e502bf67e003c5cbd07903bb85730b45120b
duy.ton <duy.ton@exe.com.vn>
2019-09-09 17:49:31 +0700

    add function to read the file

---
+++ index.js
@@ -0,0 +1,35 @@
+const ROTATE_DEG = 90;
+const EOL = require('os').EOL;
+
+var readFileSync = require('fs').readFileSync;
+
+function rotate90(matrixSqr) {
+
+}
+
+function readInputs(filePath) {
+
+    var file = readFileSync(filePath, { encoding: 'utf8' });
+    var [kTimes, ...matrix] = file.split(EOL);
+
+    for (let i = 0, nLen = matrix.length; i < nLen; i++) { 
+
+        let row = matrix[i].split(/\s+/g); 
+
+        if (nLen != row.length) { 
+            throw new Error('The program cannot process a non-square matrix!');
+        }
+
+        matrix[i] = row.map(Number);
+
+    }
+
+    return { kTimes: kTimes || 0, matrix };
+
+}
+
+var feedingFile = `${__dirname}/matrix.txt`;
+
+var data = readInputs(feedingFile);
+
+console.log(data);
