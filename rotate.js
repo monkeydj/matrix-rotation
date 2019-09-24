c17ff78830e35279a518b5961967706083478305
duy.ton <duy.ton@exe.com.vn>
2019-09-24 23:34:24 +0700

    re-organize module 2nd time with test by jest (failed by some odd reasons)

--- rotate.js
+++ rotate.js
@@ -1,50 +1,15 @@
-// export main functions
-module.exports = rotate;
-module.exports.rotateOnce = rotate90;
+const EOL = require('os').EOL;
+const rotate = require('./index');
 
-/**
- * rotate a square matrix 90deg clockwise multiple times
- * @param {number[]} sqrMatrix
- * @param {number} times
- */
-function rotate(sqrMatrix, times = 0) {
-    // since rotating multiples of 4 times results in the same beginning matrix
-    // if actualTimes is 0, then there's no rotation happened
-    var actualTimes = times % 4, rotated = Array.from(sqrMatrix);
+var kTimes = Number(process.argv[2]) || 0;
 
-    while (actualTimes-- > 0) rotated = rotate90(rotated);
+var matrix = [
+    [0, 2, 3, 4],
+    [5, 0, 7, 8],
+    [9, 10, 0, 12],
+    [13, 14, 15, 0]
+], rotated = rotate(matrix, kTimes);
 
-    return rotated;
+console.log('before:' + EOL, matrix);
+console.log(`after rotated ${kTimes} times:${EOL}`, rotated);
 
-}
-
-/**
- * rotate matrix by clockwise 90 degrees
- * @param {number[]} sqrMatrix - square matrix of some order n
- */
-function rotate90(sqrMatrix) {
-
-    var i = 0, j = 0, nLen = sqrMatrix.length;
-
-    while (i < nLen && j < nLen) {
-
-        let tmp = sqrMatrix[i][j];
-        let ri = nLen - 1 - i;
-        let rj = nLen - 1 - j;
-
-        sqrMatrix[i][j] = sqrMatrix[rj][i];
-        sqrMatrix[rj][i] = sqrMatrix[ri][rj];
-        sqrMatrix[ri][rj] = sqrMatrix[j][ri];
-        sqrMatrix[j][ri] = tmp;
-
-        // ? uncomment below line to see data of each step
-        // console.log(sqrMatrix);
-
-        if (++j >= ri) j = ++i; // go inwards to the center of matrix
-        if (i >= nLen / 2) break; // reach the center of matrix
-
-    }
-
-    return sqrMatrix;
-
-}
---
+++ rotate.test.js
@@ -0,0 +1,99 @@
+const rotate = require('./index');
+
+var inputs = {
+    square4: [
+        [0, 2, 3, 4],
+        [5, 0, 7, 8],
+        [9, 10, 0, 12],
+        [13, 14, 15, 0],
+    ],
+    square5: [
+        [0, 12, 13, 14, 15],
+        [21, 0, 23, 24, 25],
+        [31, 32, 0, 34, 35],
+        [41, 42, 43, 0, 45],
+        [51, 52, 53, 54, 0]
+    ]
+};
+
+describe('rotate a 4-by-4 matrix', () => {
+
+    test('1 times', () => {
+        let output = [
+            [13, 9, 5, 0],
+            [14, 10, 0, 2],
+            [15, 0, 7, 3],
+            [0, 12, 8, 4],
+        ];
+        expect(rotate(inputs.square4, 1)).toStrictEqual(output);
+    });
+
+    test('2 times', () => {
+        let output = [
+            [0, 15, 14, 13],
+            [12, 0, 10, 9],
+            [8, 7, 0, 5],
+            [4, 3, 2, 0],
+        ];
+        expect(rotate(inputs.square4, 2)).toStrictEqual(output);
+    });
+
+    test('3 times', () => {
+        let output = [
+            [4, 8, 12, 0],
+            [3, 7, 0, 15],
+            [2, 0, 10, 14],
+            [0, 5, 9, 13],
+        ];
+        expect(rotate(inputs.square4, 3)).toStrictEqual(output);
+    });
+
+    test('4 times', () => {
+        // as 4 times of rotation yields the original matrix
+        let output = Array.from(inputs.square4);
+        expect(rotate(inputs.square4, 4)).toStrictEqual(output);
+    });
+
+});
+
+describe('rotate a 5-by-5 matrix', () => {
+
+    test('1 times', () => {
+        let output = [
+            [51, 41, 31, 21, 0],
+            [52, 42, 32, 0, 12],
+            [53, 43, 0, 23, 13],
+            [54, 0, 34, 24, 14],
+            [0, 45, 35, 25, 15]
+        ];
+        expect(rotate(inputs.square5, 1)).toStrictEqual(output);
+    });
+
+    test('2 times', () => {
+        let output = [
+            [0, 54, 53, 52, 51],
+            [45, 0, 43, 42, 41],
+            [35, 34, 0, 32, 31],
+            [25, 24, 23, 0, 21],
+            [15, 14, 13, 12, 0]
+        ];
+        expect(rotate(inputs.square5, 2)).toStrictEqual(output);
+    });
+
+    test('3 times', () => {
+        let output = [
+            [15, 25, 35, 45, 0],
+            [14, 24, 34, 0, 54],
+            [13, 23, 0, 43, 53],
+            [12, 0, 32, 42, 52],
+            [0, 21, 31, 41, 51],
+        ];
+        expect(rotate(inputs.square5, 3)).toStrictEqual(output);
+    });
+
+    test('4 times', () => {
+        let output = Array.from(inputs.square5);
+        expect(rotate(inputs.square5, 4)).toStrictEqual(output);
+    });
+
+});
