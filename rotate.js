7c08f42f3043ba33d54aee43e01735c0caeaa9d0
duy.ton <duy.ton@exe.com.vn>
2019-09-24 21:59:33 +0700

    re-organize module; add .gitignore

--- rotate.js
+++ rotate.js
@@ -1,46 +1,50 @@
-const { EOL, readInputs } =  require('./helpers');
-const MATRIX_FILE = `${__dirname}/matrix.txt`;
-
-var matrix = readInputs(MATRIX_FILE);
-var kTimes = Number(process.argv[2]) || 0;
-
-// since rotating multiples of 4 times results in the same beginning matrix
-// if actualTimes is 0, then there's no rotation happened
-var actualTimes = kTimes % 4;
-
-console.log('before:' + EOL, matrix);
-
-while (actualTimes-- > 0) matrix = rotate90(matrix);
-
-console.log(`after rotated ${kTimes} times:${EOL}`, matrix);
+// export main functions
+module.exports = rotate;
+module.exports.rotateOnce = rotate90;
 
 /**
- * rotate matrix by 90 degrees clockwise
- * @param {string[]} matrixSqr - loaded data from matrix.txt
+ * rotate a square matrix 90deg clockwise multiple times
+ * @param {number[]} sqrMatrix
+ * @param {number} times
  */
-function rotate90(matrixSqr) {
+function rotate(sqrMatrix, times = 0) {
+    // since rotating multiples of 4 times results in the same beginning matrix
+    // if actualTimes is 0, then there's no rotation happened
+    var actualTimes = times % 4, rotated = Array.from(sqrMatrix);
 
-    var i = 0, j = 0, nLen = matrixSqr.length;
+    while (actualTimes-- > 0) rotated = rotate90(rotated);
+
+    return rotated;
+
+}
+
+/**
+ * rotate matrix by clockwise 90 degrees
+ * @param {number[]} sqrMatrix - square matrix of some order n
+ */
+function rotate90(sqrMatrix) {
+
+    var i = 0, j = 0, nLen = sqrMatrix.length;
 
     while (i < nLen && j < nLen) {
 
-        let tmp = matrixSqr[i][j];
+        let tmp = sqrMatrix[i][j];
         let ri = nLen - 1 - i;
         let rj = nLen - 1 - j;
 
-        matrixSqr[i][j] = matrixSqr[rj][i];
-        matrixSqr[rj][i] = matrixSqr[ri][rj];
-        matrixSqr[ri][rj] = matrixSqr[j][ri];
-        matrixSqr[j][ri] = tmp;
+        sqrMatrix[i][j] = sqrMatrix[rj][i];
+        sqrMatrix[rj][i] = sqrMatrix[ri][rj];
+        sqrMatrix[ri][rj] = sqrMatrix[j][ri];
+        sqrMatrix[j][ri] = tmp;
 
-        // ? uncomment below line to see data of each step 
-        // console.log(matrixSqr);
+        // ? uncomment below line to see data of each step
+        // console.log(sqrMatrix);
 
         if (++j >= ri) j = ++i; // go inwards to the center of matrix
         if (i >= nLen / 2) break; // reach the center of matrix
 
     }
 
-    return matrixSqr;
+    return sqrMatrix;
 
 }
